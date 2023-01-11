3c3b0aea62f1bcc7b21942dd57c27aee61adfe40
ezekiel <ezekiel@arcadian.garden>
2023-01-11 17:14:31 +0100

    clean code & comments

--- include/eleos.hpp
+++ include/eleos.hpp
@@ -11,19 +11,23 @@ namespace eleos {
   class interface;
 
   namespace impl {
+    // concept that matches anything that inherits from U
     template < class T, class U >
     concept derived_of = std::is_base_of< U, T >::value;
 
+    // concept that matches anything that can be converted
+    // to a std::string_view (ie. a std::string)
     template < typename T >
     concept string_like = std::is_convertible_v< T, std::string_view >;
 
-    /// Interface registry
+    // interface registry
     inline static std::unordered_map< std::string, interface * > registry = { };
   } // namespace impl
 
+  // CreateInterface return code
   enum return_code { OK = 0, FAILED };
 
-  /// interfaces
+  // interfaces
   class interface {
   public:
     interface( ) = default;
@@ -40,18 +44,20 @@ namespace eleos {
     }
   };
 
+  // exported function called by source engine
+  // to find a given interface
   extern "C" [[gnu::used]] inline auto
   CreateInterface( const char *name, int *return_code ) -> void * {
     // search through interface registry
-    auto interface =  [name]() -> void* {
+    auto interface = [ name ]( ) -> void * {
       if ( auto interface = impl::registry.find( name );
            interface != impl::registry.end( ) )
         return interface->second;
       else
         return { };
-    } ();
-    
-    // set return_code if it's valid
+    }( );
+
+    // set return_code if applicable
     if ( return_code != nullptr ) {
       *return_code = interface != nullptr ? eleos::return_code::OK
                                           : eleos::return_code::FAILED;
@@ -60,6 +66,5 @@ namespace eleos {
     return interface;
   }
 } // namespace eleos
-
 
 #endif // ELEOS_HPP
